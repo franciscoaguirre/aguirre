d17b250821b045a87774d7db80c6be00e12ae000
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-04-05 16:38:58 +0200

    feat: remove partytown :(

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind()]
 });
